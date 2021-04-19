7b2f9e97b6afb131b5b550c946b01ea5cc3e1799
Nathalie <natgo22@gmail.com>
2021-04-18 21:14:51 -0400

    updated nav

--- js/script.js
+++ js/script.js
@@ -4,18 +4,21 @@ function start(){
 
    // $("body").css("background-color", "pink");
 
-        
+    $("nav a:first").click();
+
 }
 
 
 
 $(document).on ("click", "#nav a[href]", nav); 
 
-function nav(e) {
+function nav(e){
 
     //e.preventDefault();
 
     var href = $(this).attr("href");
-    alert(href);
-    //$("#content").load("modules/" + href);
+    $("#content").load("modules/" + href);
+
+    $("#nav a").fadeTo("fast", 0.25);
+    $(this).fadeTo("fast", 1);
 }
